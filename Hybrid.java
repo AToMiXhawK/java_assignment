52718b1dc22052c20a024b0712359b048bcbb939
AToMiXhawK <githinmanesh@gmail.com>
2019-03-22 11:38:15 +0530

    Assignment Q1

--- Hybrid.java
+++ Hybrid.java
@@ -4,7 +4,7 @@ class Hybrid
 {
     public static void main(String[] args) 
     {
-        Result S1 = new Result("Githin", 101, 30, 35, 35);
+        Result S1 = new Result("hawk", 101, 30, 35, 35);
         S1.display_total();
     }
 
