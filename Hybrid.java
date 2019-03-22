136d8347ba2490feab0d2807b2a48bf2c6d3977f
AToMiXhawK <githinmanesh@gmail.com>
2019-03-22 14:50:43 +0530

    Assignment Q1

--- Hybrid.java
+++ Hybrid.java
@@ -8,7 +8,7 @@ class Hybrid
         Result S2 = new Result("kryptiksage", 102, 35, 30, 32);
         Result S3 = new Result("merothh", 103, 32, 34, 34);
         Result S4 = new Result("vaskian", 104, 40, 37, 30);
-        Result S5 = new Result("elekdra", 105, 36, 34, 32);
+        Result S5 = new Result("elekdra",105,35,42,30);
 
         S1.display_total();
         S2.display_total();
