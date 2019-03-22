20a58b782c6210aa43d665aa1d7997b955df05cc
AToMiXhawK <githinmanesh@gmail.com>
2019-03-22 12:10:05 +0530

    Assignment Q1

--- Hybrid.java
+++ Hybrid.java
@@ -4,8 +4,17 @@ class Hybrid
 {
     public static void main(String[] args) 
     {
-        Result S1 = new Result("hawk", 101, 30, 35, 35);
+        Result S1 = new Result("atomixhawk", 101, 30, 35, 35);
+        Result S2 = new Result("kryptiksage", 102, 35, 30, 32);
+        Result S3 = new Result("merothh", 103, 32, 34, 34);
+        Result S4 = new Result("vaskian", 104, 40, 37, 30);
+        Result S5 = new Result("elekdra", 105, 36, 34, 32);
+
         S1.display_total();
+        S2.display_total();
+        S3.display_total();
+        S4.display_total();
+        S5.display_total();
     }
 
 }
