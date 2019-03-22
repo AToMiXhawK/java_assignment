dedf317b74f005ee97f27d2f7c592dbb190a6061
AToMiXhawK <githinmanesh@gmail.com>
2019-03-22 14:51:23 +0530

    Hybrid.java with user input

---
+++ Hybrid_1.java
@@ -0,0 +1,21 @@
+import studpack.*;
+
+class Hybrid_1
+{
+    public static void main(String[] args) 
+    {
+        Result S1 = new Result();
+        Result S2 = new Result();
+        Result S3 = new Result();
+        Result S4 = new Result();
+        Result S5 = new Result();
+
+        S1.display_total();
+        S2.display_total();
+        S3.display_total();
+        S4.display_total();
+        S5.display_total();
+    }
+
+}
+
