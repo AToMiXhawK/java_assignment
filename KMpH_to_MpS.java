8067e54955e3a173b6fe57d0d56193b977156d90
Aravind013 <aravind.sasi.8@gmail.com>
2019-05-07 21:46:07 +0530

    Km/h to m/s applet

--- KMpH_to_MpS.java
+++ KMpH_to_MpS.java
@@ -4,26 +4,42 @@ import java.awt.event.*;
 
 public class KMpH_to_MpS extends Applet implements ActionListener
 {
-   TextField kmph, mps, resultNum;
+   TextField kmph, mps;
    public KMpH_to_MpS()
    {                      
       setLayout(new GridLayout(3, 2, 10, 15));   
       kmph = new TextField(15);
-      resultNum = new TextField(15); 
-      kmph.addActionListener(this); 
-      add(new Label("Enter speed in km/hr: "));  
+      mps = new TextField(15); 
+      kmph.addActionListener(this);  
+      add(new Label("Enter speed in kmph: "));  
       add(kmph);
 
-      add(new Label("Speed in m/s: "));              
-      add(resultNum);
+      add(new Label("Speed in mps: "));              
+      add(mps);
+      mps.addActionListener(this);
    }
    public void actionPerformed(ActionEvent e)
-   {                        
-      String str1 = kmph.getText();
+   {  
+      Boolean b1 = kmph.getText().isEmpty();
+      Boolean b2 = mps.getText().isEmpty();
+      if(b2)
+      {
+        String str1 = kmph.getText();
+        double fn1 = Double.parseDouble(str1);
+        double s1 = fn1/3.6;
+        mps.setText("" + s1);
+      }
 
-      double fn = Double.parseDouble(str1);
-      double s = fn/3.6;
+      if(b1)
+      {
+        String str2 = mps.getText();
+        double fn2 = Double.parseDouble(str2);
+        double s2 = fn2*3.6;
+        kmph.setText("" + s2);
+      }
+      
+     
  
-      resultNum.setText("" + s);
+      
    }
 }
